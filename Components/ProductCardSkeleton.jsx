d188a51d5fae6a8f43960091e2707b5ac1b37fee
Shippu Kumar <Itzshippu@gmail.com>
2023-12-16 15:17:32 +0530

    Fixed Error

---
+++ Components/ProductCardSkeleton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductCardSkeleton = () => {
+  return (
+    <div>ProductCardSkeleton</div>
+  )
+}
+
+export default ProductCardSkeleton
--- app/adb-butinaca/page.js
+++ app/adb-butinaca/page.js
@@ -1,4 +1,4 @@
-import ProductCard from '@/Components/ProductCard'
+import ProductCard from '/Components/ProductCard'
 import React from 'react'
 
 const page = () => {
